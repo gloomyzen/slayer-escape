8e2cede223f49404f01b8261c50657b78f89f955
Artur Barilo <faustkeller45@gmail.com>
2021-04-22 22:00:01 +0300

    master | wip - wip map generate

--- src/battleModule/battleField.cpp
+++ src/battleModule/battleField.cpp
@@ -2,6 +2,7 @@
 #include "common/debugModule/logManager.h"
 #include "databasesModule/databaseManager.h"
 #include "databasesModule/mapsDatabase.h"
+#include "databasesModule/tilesDatabase.h"
 #include <map>
 #include <tuple>
 
@@ -23,6 +24,16 @@ void battleField::initLayer(int id) {
         //todo log error
         return;
     }
-    std::map<std::pair<int, int>, cocos2d::Node*> result;
-    //todo wip
+    std::vector<int> ground;
+    std::vector<int> wall;
+    auto tilesDb = GET_DATABASE_MANAGER().getDatabase<tilesDatabase>(databaseManager::eDatabaseList::TILES_DB);
+    for (const auto& tileId : map->fill) {
+        tilesDb->getTileById(tileId);
+    }
+    std::map<int, std::map<int, cocos2d::Node*>> result;
+    for (int x = 0; x < map->width; ++x) {
+        for (int y = 0; y < map->height; ++y) {
+            result[x][y] = new cocos2d::Node();
+        }
+    }
 }
