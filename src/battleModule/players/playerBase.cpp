2d4dce4c0787bcbc85e5ad61de50a10dd931f44a
Artur Barilo <faustkeller45@gmail.com>
2021-04-25 22:23:39 +0300

    master | wip player

---
+++ src/battleModule/players/playerBase.cpp
@@ -0,0 +1,1 @@
+#include "playerBase.h"
---
+++ src/battleModule/players/playerBase.h
@@ -0,0 +1,20 @@
+#ifndef SLAYER_ESCAPE_PLAYERBASE_H
+#define SLAYER_ESCAPE_PLAYERBASE_H
+
+
+#include "cocos2d.h"
+#include "common/coreModule/nodes/nodeProperties.h"
+#include <string>
+
+namespace mb::battleModule {
+    class playerBase
+        : public common::coreModule::nodeProperties
+        , public cocos2d::Sprite {
+      public:
+        playerBase();
+        ~playerBase();
+    };
+}// namespace mb::battleModule
+
+
+#endif// SLAYER_ESCAPE_PLAYERBASE_H
