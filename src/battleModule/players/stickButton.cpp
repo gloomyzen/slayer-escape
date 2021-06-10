b5469736c1459efa38ee65632babbf842d2537b5
Artur Barilo <faustkeller45@gmail.com>
2021-06-10 23:38:54 +0300

    master(mvp) | wip: players, update stick

--- src/battleModule/players/stickButton.cpp
+++ src/battleModule/players/stickButton.cpp
@@ -53,12 +53,12 @@ void stickButton::initController() {
             auto newPos = cocos2d::Vec2(startPos.x - btn->getContentSize().width / 2,
                                         startPos.y - btn->getContentSize().height / 2);
             btn->setPosition(newPos);
-            smallBtn->setPosition(cocos2d::Vec2::ZERO);
+            smallBtn->setAnchorPoint(cocos2d::Vec2::ZERO);
         } break;
         case Widget::TouchEventType::MOVED: {
             auto currentTouchPos = getTouchMovePosition();
             auto newPos = cocos2d::Vec2(currentTouchPos.x - startPos.x, currentTouchPos.y - startPos.y);
-            while (newPos.distance(cocos2d::Vec2::ZERO) > stickDistance) {
+            /*while (newPos.distance(cocos2d::Vec2::ZERO) > stickDistance) {
                 if (newPos.x > 0.f) {
                     newPos.x -= .1f;
                 } else {
@@ -70,7 +70,7 @@ void stickButton::initController() {
                     newPos.y += .1f;
                 }
             }
-            smallBtn->setPosition(newPos);
+            smallBtn->setPosition(newPos);*/
             getEmitter()->onMove.emit(newPos.x, newPos.y);
         } break;
         case Widget::TouchEventType::ENDED:
@@ -81,17 +81,18 @@ void stickButton::initController() {
                 auto delay = cocos2d::DelayTime::create(waitAfterEndTouch);
                 auto clb = cocos2d::CallFunc::create([this]() {
                     btn->setPosition(cocos2d::Vec2::ZERO);
-                    smallBtn->setPosition(cocos2d::Vec2::ZERO);
+                    smallBtn->setAnchorPoint(cocos2d::Vec2::ZERO);
                 });
                 auto seq = cocos2d::Sequence::create(delay, clb, nullptr);
                 seq->setTag(static_cast<int>(stickButton::eActionType::WAIT_AFTER_END_TOUCH));
                 runAction(seq);
 
                 {
+                    //todo remove stickInertionTime
                     //move stick
-                    auto move = cocos2d::MoveTo::create(stickInertionTime, cocos2d::Vec2::ZERO);
-                    move->setTag(static_cast<int>(stickButton::eActionType::WAIT_AFTER_END_TOUCH));
-                    smallBtn->runAction(move);
+//                    auto move = cocos2d::MoveTo::create(stickInertionTime, cocos2d::Vec2::ZERO);
+//                    move->setTag(static_cast<int>(stickButton::eActionType::WAIT_AFTER_END_TOUCH));
+//                    smallBtn->runAction(move);
                 }
             }
             return true;
