8740607c98fc9bb4e34f8591f78b70de7d66ff73
Artur Barilo <faustkeller45@gmail.com>
2021-06-24 23:18:33 +0300

    master(mvp) | tmx support - update a new level

--- Resources/images/battle/maps/location_2/props.tsx
+++ Resources/images/battle/maps/location_2/props.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="props" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="textures/props.png" trans="ff00ff" width="512" height="512"/>
+ <tile id="138">
+  <properties>
+   <property name="playerSpawn" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="156">
+  <properties>
+   <property name="enemySpawn" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
--- Resources/images/battle/maps/location_2/struct.tsx
+++ Resources/images/battle/maps/location_2/struct.tsx
@@ -1,4 +1,175 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="struct" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="textures/struct.png" trans="ff00ff" width="512" height="512"/>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="false"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="87">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="100">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="113">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="119">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="32">
+    <properties>
+     <property name="wall" type="bool" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
